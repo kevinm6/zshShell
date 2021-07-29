09213060e252bc89d4d2082f3af955b49a5f1873
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-07-29 17:33:44 +0200

    Split some configuration of zshrc in separate files

---
+++ binds.zsh
@@ -0,0 +1,27 @@
+#############################################################
+#----------------------- BINDKEYS --------------------------#
+#############################################################
+
+# Build 290721
+
+# vi mode
+bindkey -M vicmd "j" vi-down-line-or-history
+bindkey -M vicmd "k" vi-up-line-or-history
+
+# cmd
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "<leader>\033b" backward-word
+bindkey "<leader>\033f" forward-word
+
+# alt
+bindkey "\033b" backward-word
+bindkey "\033f" forward-word
+
+# ctrl
+bindkey "^u" backward-kill-line
+bindkey "^w" backward-kill-word
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
+
+
---
+++ options.zsh
@@ -0,0 +1,20 @@
+#############################################################
+#-----------------------  OPTIONS  -------------------------#
+#############################################################
+
+# Build 290721
+
+setopt auto_cd
+setopt auto_menu
+setopt auto_list
+setopt correct
+setopt vi
+setopt no_list_beep
+# History options
+setopt hist_ignore_all_dups # remove older duplicate entries from history
+setopt hist_reduce_blanks # remove superfluous blanks from history items
+setopt share_history # share shell history between different shells
+setopt append_history # add to main history instead of overwrite it
+setopt hist_save_no_dups # do not save duplicates
+
+
