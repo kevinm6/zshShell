3d115f7d495d4f5dbf025a1d39594954346a4ec7
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-10-15 09:17:54 +0200

    Update aliases as in iTerm2, removing the git ones

--- aliases.zsh
+++ aliases.zsh
@@ -2,17 +2,19 @@
 #------------------------  ALIAS  --------------------------#
 #############################################################
 
-# Version 29.07.21
+# Version 15.10.21 - 9:00
 
 # ------------------------- SUFFIX ALIASES -------------------------
 alias -s json=subl
 alias -s txt=vim
 alias -s md=vim
 alias -s pdf=preview
+alias -s m3u8=iina
 
 # ------------------------- VARIOUS TERMINAL -------------------------
 alias l="ls -al"
 alias c="clear"
+alias cdc="cd && clear"
 alias ".."="cd .."
 alias "..,.."="cd ../.."
 alias :q="exit"
@@ -24,38 +26,20 @@ alias ka="killall"
 alias rmh="rm $ZDOTDIR/.zsh_history"
 alias zpath="$ZDOTDIR"
 alias vipath="$HOME/Documents/Devices/Backup_Files/Shell/vim/"
-alias cppath="echo `pwd` | pbcopy"
+alias cppath="echo $PWD | pbcopy"
 alias binbackuppath="$HOME/Documents/Devices/Backup_Files/Shell/bin/"
 alias zreload="source $ZDOTDIR"
-alias vi=vim
+alias python="/usr/local/opt/python/libexec/bin/python/"
 
 # ------------------------- APPS -------------------------
 alias preview="open -a Preview"
 alias safari="open -a Safari"
 alias photos="open -a Photos"
+alias iina="open -a IINA"
 
 # ------------------------- HTOP STUFF -------------------------
 alias cpu="(which htop > /dev/null && htop --sort-key PERCENT_CPU) || top -o cpu"
 alias ram="(which htop > /dev/null && htop --sort-key RES) || top -o rsize"
-
-# ------------------------- GIT -------------------------
-alias gi="git init"
-alias gs="git status"
-alias gf="git fetch"
-alias gp="git push"
-alias gb="git branch"
-alias grm="git rm"
-alias gpl="git pull"
-alias gsw="git switch" 
-alias gc="git commit -m" # git commit w/ message
-alias ga="git add"
-alias gaa="git add ." # git add all changes in current dir
-alias gb="git branch"
-alias gck="git checkout"
-alias gdf="git diff"
-alias gdt="git difftool"
-alias gl="git log"
-alias glo="git log --oneline"
 
 # ------------------------- GO -------------------------
 alias gor="go run"
