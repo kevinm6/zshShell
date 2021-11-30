d5a593e323f5e8bfe6ff39d352228cff7dc6cf83
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-11-30 21:37:42 +0100

    Improve prompt and git integration

--- prompt.zsh
+++ prompt.zsh
@@ -3,29 +3,44 @@
 # Description: K ZSH Shell prompt
 # Author: Kevin
 # Source: https://github.com/kevinm6/zsh/
-# Last Modified: 30/11/2021 - 17:35
+# Last Modified: 30.11.21 21:35
 #-------------------------------------------
 
-precmd() {
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git*' formats "%{$fg_bold[red]%}on  %b %m %{$reset_color%} %{$fg[yellow]%}%u%f%{$reset_color%} %{$fg[green]%}%c%f%{$reset_color%} "
+zstyle ':vcs_info:git*' actionformats '%s  %r/%S %b (%a) %m %u %c '
+zstyle ':vcs_info:*' check-for-changes true
+
+precmd() { 
+	vcs_info
+
 	[[ -z ${vcs_info_msg_0_} ]] && {
 		# No information from vcs_info
 		# Increase verbosing parent dirs
-		PROMPT=$'\n  %# %F{blue}%6%f\n '
+		pathlen=6
+		msg=""
 	} || {
-		PROMPT=$'\n  %# %F{blue}%3~%f\t%B%F{red} ${vcs_info_msg_0_}%f%b\n '
+		msg=$' ${vcs_info_msg_0_}'
+		pathlen=3
 	}
 }
 
+# PROMPT=$'\n  %# %F{blue}%$pathlen~%f\t%B%F{red} ${vcs_info_msg_0_}%f%b\n'
+PROMPT=$'\n %# %F{blue}%$pathlen~%f\t${vcs_info_msg_0_}\n '
+
 function zle-line-init zle-keymap-select {
 		case ${KEYMAP} in
-      (vicmd)      VI_MODE="%F{yellow\}-- NORMAL --%f" ;;
+      (vicmd)      VI_MODE="%F{yellow}-- NORMAL --%f" ;;
       (main|viins) VI_MODE="%F{blue}-- INSERT --%f" ;;
       (*)          VI_MODE="%F{blue}-- INSERT --%f" ;;
     esac
-    RPROMPT="$VI_MODE ⌚️ %F{white}%*%f"
+    RPROMPT='$VI_MODE  ⌚️ %F{white}%*%f'
     zle reset-prompt
 }
 
 zle -N zle-line-init
 zle -N zle-keymap-select
+
+setopt prompt_subst
 
