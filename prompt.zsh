039f7039a5306bdb5b64b6153170f5d5afeb315f
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-11-30 17:45:36 +0100

    Cleanup and improve generating prompt

--- prompt.zsh
+++ prompt.zsh
@@ -3,47 +3,29 @@
 # Description: K ZSH Shell prompt
 # Author: Kevin
 # Source: https://github.com/kevinm6/zsh/
-# Last Modified: 25.11.21 11:46 
+# Last Modified: 30/11/2021 - 17:35
 #-------------------------------------------
 
-
 precmd() {
-	vcs_info
-} 
-
-# Vim Modes
-vi_mode_ins=${vi_mode_ins:-'%F{blue}[ ins ]%f'}
-vi_mode_cmd=${vi_mode_cmd:-'%F{yellow}[ cmd ]%f'}
-vi_mode_symbol="${vi_mode_ins}" #initialize mode
-
-
-zle-keymap-select() { # on keymap change, define the mode and redraw prompt
-	if [ "${KEYMAP}" = 'vicmd' ]; then
-		vi_mode_symbol="${vi_mode_cmd}"
-	else
-		vi_mode_symbol="${vi_mode_ins}"
-	fi
-	zle reset-prompt
+	[[ -z ${vcs_info_msg_0_} ]] && {
+		# No information from vcs_info
+		# Increase verbosing parent dirs
+		PROMPT=$'\n  %# %F{blue}%6%f\n '
+	} || {
+		PROMPT=$'\n  %# %F{blue}%3~%f\t%B%F{red} ${vcs_info_msg_0_}%f%b\n '
+	}
 }
+
+function zle-line-init zle-keymap-select {
+		case ${KEYMAP} in
+      (vicmd)      VI_MODE="%F{yellow\}-- NORMAL --%f" ;;
+      (main|viins) VI_MODE="%F{blue}-- INSERT --%f" ;;
+      (*)          VI_MODE="%F{blue}-- INSERT --%f" ;;
+    esac
+    RPROMPT="$VI_MODE ⌚️ %F{white}%*%f"
+    zle reset-prompt
+}
+
+zle -N zle-line-init
 zle -N zle-keymap-select
-
-# reset to default mode at the end of line input reading
-zle-line-finish() {
-	vi_mode_symbol="${vi_mode_symbol}"
-}
-zle -N zle-line-finish
-
-TRAPINT() {
-	vi_mode_symbol="${vi_mode_ins}"
-	return $(( 128 + $1 ))
-}
-
-parse_git_branch() {
-	 git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ - \1 -/'
-}
-
-setopt PROMPT_SUBST
-
-RPROMPT=$'$vi_mode_symbol  ⌚️ %F{white}%*%f'
-PROMPT=$'\n  %# %F{blue}%3~%f\t%B%F{red}⅄$(parse_git_branch)%f%b\n '
 
